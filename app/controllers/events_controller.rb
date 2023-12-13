738dcba587dea87ff6de6d77507007a3f5e9cc90
keita <keitanakanishi5115@icloud.com>
2023-12-13 12:33:05 +0900

    Add_イベント詳細機能追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,8 @@ class EventsController < ApplicationController
     def index
         @events = Event.all.includes(:user).order(created_at: :desc).page(params[:page])
     end
+
+    def show
+        @event = Event.find(params[:id])
+    end
 end
