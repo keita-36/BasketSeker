16196dfedce6c6961ee7b46f61d2f0508e0781ff
keita <keitanakanishi5115@icloud.com>
2023-12-19 11:21:33 +0900

    Add_ランキング機能

--- app/models/match_result.rb
+++ app/models/match_result.rb
@@ -9,4 +9,29 @@ class MatchResult < ApplicationRecord
       where(event_id: event.id, user_1_id: [user1.id, user2.id], user_2_id: [user2.id, user1.id])
     ).exists?
   end
+
+  def self.match_count
+    User.all.map do |user|
+      [user, where(user_1: user).or(where(user_2: user)).count]
+    end.to_h
+  end
+
+  def self.win_count
+    User.all.map do |user|
+      [user, where(winner: user).count]
+    end.to_h
+  end
+
+  def self.win_rate
+    match_counts = self.match_count
+    win_counts = self.win_count
+
+    User.all.map do |user|
+      match_count = match_counts[user]
+      win_count = win_counts[user]
+
+      win_rate = match_count.zero? ? 0 : (win_count.to_f / match_count * 100).round(2)
+      [user, win_rate]
+    end.to_h
+  end
 end
