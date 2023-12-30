864923e02fa136462e027ea7697cdbb9fefca8c8
keita <keitanakanishi5115@icloud.com>
2023-12-30 13:26:10 +0900

    Update_イベント作成ビュー

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module BasketSeker
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
+    config.active_record.default_timezone = :local
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
