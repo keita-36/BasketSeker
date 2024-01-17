e1af72084c5dce48cf7c1602e39584381518903f
keita-36 <131235353+keita-36@users.noreply.github.com>
2024-01-17 22:08:41 +0900

    fly.io設定完了

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "https://basketseker.fly.dev"]
-  # ActionCable.server.config.disable_request_forgery_protection = true
+  ActionCable.server.config.disable_request_forgery_protection = true
   # config.action_cable.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
